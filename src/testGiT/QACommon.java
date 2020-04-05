29a3509d7bfdc964627e04dba4a354fefd2ae9ef
Piyush Automation <piyushgoyal29@gmail.com>
2020-04-05 16:17:16 +0530

    QA2 second commit

--- src/testGiT/QACommon.java
+++ src/testGiT/QACommon.java
@@ -2,7 +2,8 @@ package testGiT;
 
 public class QACommon {
 	public static void main(String[] args) {
-		System.out.println("Method by QA1");;
+		System.out.println("Method by QA1");
+		System.out.println("Method by QA2");
 	}
 
 }
